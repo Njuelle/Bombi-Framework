94131815dcd425e18b46a8ed2263db302299938a
njuelle <n.juelle@gmail.com>
2017-11-15 13:16:51 +0100

    moving repo to github

---
+++ app/Controllers/BeerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Beer;
+
+class BeerController extends Abstracted\Controller
+{
+    public function getAll($vars)
+    {
+        //get all beers with orm
+        $beers = Beer::all();
+        
+        //each controllers has a view attribut, 
+        //use it to render your template 
+        echo $this->view->display('partials/beers.twig', array(
+            'beers' => $beers
+        ));
+    }
+
+    public function getOne($vars)
+    {
+        //use $vars to get routes query params
+        $beer = Beer::find($vars['id']);
+
+        echo $this->view->display('partials/beer.twig', array(
+            'beer' => $beer
+        ));
+    }
+}
